38dc76c496e52f272146443ba02418911fd75d27
Aleh Atsman <aleh.atsman@gmail.com>
2019-06-03 10:32:11 +0200

    Simple parity implementation

--- src/primitives/__test__/parity.spec.ts
+++ src/primitives/__test__/parity.spec.ts
@@ -1,5 +1,15 @@
 import parity from "@/primitives/parity";
+import { forEach } from "@/std";
 
 test("parity", () => {
-  expect(parity(0)).toBe(0);
+  const testCases = [
+    { input: 0, output: 0 },
+    { input: 1, output: 1 },
+    { input: 2, output: 1 },
+    { input: 3, output: 0 },
+  ];
+
+  forEach((testCase) => {
+    expect(parity(testCase.input)).toBe(testCase.output);
+  }, testCases);
 });
--- src/primitives/parity.ts
+++ src/primitives/parity.ts
@@ -1,5 +1,17 @@
+/**
+ * parity - counts number of bits which are 1s.
+ * If the number is odd than it returns 1
+ * otherwise returns 0.
+ */
 function parity(x: number): number {
-  return x;
+  let i = x;
+  let c = 0;
+  while (i > 0) {
+    c += i & 1; // tslint:disable-line
+    i = i >> 1; // tslint:disable-line
+  }
+
+  return c % 2 === 0 ? 0 : 1;
 }
 
 export default parity;
