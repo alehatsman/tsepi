fc0a0905a32b0b60bb69f78aabbdbdfa15d50aa7
Aleh Atsman <aleh.atsman@gmail.com>
2019-06-04 17:32:29 +0200

    Anonymous letter solution

---
+++ src/hash_tables/__tests__/anonymousLetter.spec.ts
@@ -0,0 +1,16 @@
+import { forEach } from "@/std";
+import anonymousLetter from "../anonymousLetter";
+
+test("anonymousLetter", () => {
+  const testCases = [
+    { l: "", m: "", res: true },
+    { l: "a", m: "", res: false },
+    { l: "abc", m: "abc", res: true },
+    { l: "abc", m: "abcd", res: true },
+    { l: "aabc", m: "aabcd", res: true },
+  ];
+
+  forEach(({ l, m, res }) => {
+    expect(anonymousLetter(l, m)).toBe(res);
+  }, testCases);
+});
---
+++ src/hash_tables/anonymousLetter.ts
@@ -0,0 +1,52 @@
+import { filter, reduce } from "@/std";
+
+const letterOnlyRegex = new RegExp("[A-Za-z]");
+
+function isLetter(ch: string) {
+  return letterOnlyRegex.test(ch);
+}
+
+function splitText(text: string) {
+  const allLetters = text.split("");
+  const onlyTextLetters = filter((ch) => {
+    return !!ch && isLetter(ch);
+  }, allLetters);
+  return onlyTextLetters;
+}
+
+function countLetters(letters: string[]) {
+  return reduce(
+    (acc, ch) => {
+      if (!acc[ch]) {
+        acc[ch] = 1;
+      }
+      acc[ch] += 1;
+      return acc;
+    },
+    letters,
+    {} as { [key: string]: number },
+  );
+}
+
+export default function anonymousLetter(
+  letter: string,
+  magazine: string,
+): boolean {
+  const letterLetters = splitText(letter);
+  const magazineLetters = splitText(magazine);
+
+  if (letterLetters.length > magazineLetters.length) {
+    return false;
+  }
+
+  const llc = countLetters(letterLetters);
+  const mmc = countLetters(magazineLetters);
+
+  for (const ch of letterLetters) {
+    if (llc[ch] > mmc[ch]) {
+      return false;
+    }
+  }
+
+  return true;
+}
